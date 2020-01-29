80cf210bc0f083ff80fa71ab200eca0f3cc6abce
lmcconnell1665 <56060247+lmcconnell1665@users.noreply.github.com>
2020-01-29 11:40:27 -0500

    Added stylesheet/default team selection
    Added an external stylesheet and comments. Also set Tennessee as the default selection when the dashboard is opened.

--- app.py
+++ app.py
@@ -7,12 +7,14 @@ import numpy as np
 import datetime
 
 # reading in data.frame
-ratings_df = pd.read_csv("TV_Ratings_onesheet.csv")  # <----- make sure you have this file in the appropriate folder.
-# Makes the date column a column of dates ... a type thing...
+ratings_df = pd.read_csv("TV_Ratings_onesheet.csv")
+
+# Makes the date column a column of dates
 ratings_df.Date = pd.to_datetime(ratings_df.Date)
 
 # Drops some silly columns
 ratings_df.drop(columns = ["GameID", "TeamIDsDate"])
+
 # Sorting the df makes the plots turn out nicer in the case you want to use lines.
 ratings_df = ratings_df.sort_values(by = "Date")
 
@@ -22,23 +24,25 @@ ratings_df = ratings_df.loc[currentGames]
 
 # Getting the list of unique Home Team names
 teamNames = ratings_df["Home Team"].unique()
-otherTeamIndex = np.isin(teamNames, ["Alabama", "Tennessee"], invert = True)
-# Creating an object for "options" in the dropdown menu.  It is all team names except Alabama and Tennessee (UT)
-teamNamesDict = [{'label' : tn, 'value': tn} for tn in teamNames[otherTeamIndex]]
+
+# Creating an object for "options" in the dropdown menu.
+teamNamesDict = [{'label' : tn, 'value': tn} for tn in teamNames]
+
+#Adding external stylesheet
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 # Create the app
-app = dash.Dash(__name__)
-
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 # Create the layout
 app.layout = html.Div(
     # children =
-    [html.H1('College Football'),
-     html.P(['Choose a Team!',
-            dcc.Dropdown(id='First_Dropdown', options=teamNamesDict, multi=True, value=[] ) ]
+    [html.H1('College Football'), #Dashboard title
+     html.P(['Choose a Team.', #Dropdown instructions
+            dcc.Dropdown(id='First_Dropdown', options=teamNamesDict, multi=True, value=['Tennessee'] ) ] #Team selection dropdown
             , style={'width':'400px'}),
-     dcc.Graph( id = 'First_Graph'),
-     dcc.RadioItems(
+     dcc.Graph( id = 'First_Graph'), #TV attendance scatterplot
+     dcc.RadioItems( #Home, away, both selection button
             id='home-away',
             options=[{'label': i, 'value': i} for i in ['Home', 'Away', 'Both']],
             value='Home',
@@ -48,19 +52,16 @@ app.layout = html.Div(
     ] # Closes out the children of outermost html.Div of app.layout
 ) # closes app.layout outermost html.Div 
 
+# Create the callbacks here
 
-# Create the callbacks here...
 @app.callback(
     Output(component_id='First_Graph', component_property='figure'),
     [Input(component_id='First_Dropdown', component_property='value'),
     Input(component_id='home-away', component_property='value')]
 )
-def update_figure(teamX, Radio_Selection):
-    print(teamX)
-    print(Radio_Selection)
-    
 
-    
+def update_figure(teamX, Radio_Selection):
+
     data = []
     for team in teamX:
         if Radio_Selection == 'Home':
@@ -72,7 +73,6 @@ def update_figure(teamX, Radio_Selection):
         elif Radio_Selection == 'Both':
             Date = ratings_df[(ratings_df["Home Team"] == team) | (ratings_df["Visitor Team"] == team)]["Date"]
             Viewers = ratings_df[(ratings_df["Home Team"] == team) | (ratings_df["Visitor Team"] == team)]["VIEWERS"]         
-            
             
         data.append( dict(
                     type = "scatter",
@@ -87,11 +87,9 @@ def update_figure(teamX, Radio_Selection):
                     hovertext = ratings_df[(ratings_df["Home Team"] == team) | (ratings_df["Visitor Team"] == team)]["GAME"]
                     ) )
     
-    fig = { 'data': data
-            }
+    fig = { 'data': data}
     
     return fig
-
 
 if __name__ == "__main__":
     app.run_server(debug=True)
